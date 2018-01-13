8a31dc502bf587da3bc254f6ec11c9e1be219b0d
Zane Muir <zane@Zanes-MacBook-Pro.local>
2018-01-13 15:27:11 +0800

    better weight

---
+++ newWeightInfo.sh
@@ -0,0 +1,25 @@
+#示例：
+#python3 limeWeight.py --nw C1M1 22.34 C1M2 22.32 C1M3 17.88 C1M4 16.22 C1M5 18.99 C1M6 32.22 C1M7 12.23 C1M8 19.00
+#小鼠编号 体重
+
+
+#N1
+#python3 limeWeight.py --nw C1M1 C1M2 C1M3 C1M4 C1M5 C1M6 C1M7 C1M8
+
+#H1
+#python3 limeWeight.py --nw C2M1 C2M2 C2M3 C2M4 C2M5 C2M6 C2M7 C2M8
+
+#H2
+#python3 limeWeight.py --nw C3M1 C3M2 C3M3 C3M4 C3M5 C3M6 C3M7 C3M8 C3M9
+
+#N2
+#python3 limeWeight.py --nw C4M1 C4M2 C4M3 C4M4 C4M5 C4M6 C4M7 C4M8 C4M9
+
+#N3
+#python3 limeWeight.py --nw C5M1 C5M2 C5M3 C5M4 C5M5 C5M6 C5M7 C5M8
+
+#H3
+#python3 limeWeight.py --nw C6M1 C6M2 C6M3 C6M4 C6M5 C6M6 C6M7 C6M8
+
+#H4
+#python3 limeWeight.py --nw C7M1 C7M2 C7M3 C7M4 C7M5 C7M6 C7M7 C7M8
